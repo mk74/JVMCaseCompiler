01142857ce9dc69761ab66558f279a313c43902b
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-25 22:11:36 +0100

    Arithmetic operations on nested expressions

--- CaseCompiler/compiler.hs
+++ CaseCompiler/compiler.hs
@@ -6,7 +6,7 @@ data CExpr = CEInt Int
 			 | CEop CExpr COp CExpr
 
 stack_load :: CExpr -> CExpr -> String
-stack_load (CEInt i1) (CEInt i2) = "sipush " ++ show i1 ++ "\nistore_1\n" ++ "sipush " ++ show i2 ++ "\nistore_2\n" ++"iload_1\niload_2\n"
+stack_load (CEInt i1) (CEInt i2) = "sipush " ++ show i1 ++ "\nsipush " ++ show i2 ++ "\n"
 
 op_func :: String -> String
 op_func "+" = "iadd"
@@ -14,44 +14,51 @@ op_func "-" = "isub"
 op_func "*" = "imul"
 op_func "/" = "idiv"
 
+--op_func "<" = 
+--op_func "==" = ""
+op_func "and" = "iand"
+op_func "or" = "ior"
+
 compile :: CExpr -> String
 compile (CEInt i1) = show i1
 compile (CEBool b1) = show b1
 compile (CEString str1) = str1
 
-compile (CEop e1 op1 e2) = (stack_load e1 e2) ++ (op_func op1) ++ "\n"
-
-{-
-compile (CEop e1 "<" e2) = "Caciek"
-compile (CEop e1 "==" e2) = "Caciek"
-compile (CEop e1 "and" e2) = "Caciek"
-compile (CEop e1 "or" e2) = "Caciek"-}
+compile (CEop (CEInt i1) op1 (CEInt i2)) = (stack_load (CEInt i1) (CEInt i2)) ++ (op_func op1) ++ "\n"
+compile (CEop expr1 op1 expr2) = (compile expr1) ++ (compile expr2) ++ (op_func op1) ++ "\n"
 
 --compile _ = "Maciek"
 
 preamble :: String
-preamble = ".class public Program.program\n.super java/lang/Object\n\n"
+preamble = ".class public Program\n.super java/lang/Object\n\n"
 			++ ".method public <init>()V\n"
 			++ "aload_0\ninvokenonvirtual java/lang/Object/<init>()V\n"
   			++ "return\n.end method\n\n"
 
 static_main_start :: String
 static_main_start = ".method public static main([Ljava/lang/String;)V\n"
-  					++ ".limit stack 5\n.limit locals 5\n"
+  					++ ".limit stack 100\n.limit locals 100\n"
 
 static_main_end :: String
-static_main_end = "istore_3\n"
+static_main_end = "istore 99\n"
 				  ++"getstatic     java/lang/System/out Ljava/io/PrintStream;\n"
- 				  ++ "iload_3\n"
+ 				  ++ "iload 99\n"
   				  ++ "invokevirtual java/io/PrintStream/println(I)V\n"
   				  ++ "return\n.end method"
 
 jasminWrapper :: String -> String
 jasminWrapper str1 = preamble ++ static_main_start ++ str1 ++ static_main_end
 
-test1 :: CExpr
+--testing addition two numbers
 test1 = (CEop (CEInt 10) "+" (CEInt 15))
 
-main = putStrLn (jasminWrapper (compile test1))
+--testing AND operator
+test2 = (CEop (CEInt 2) "and" (CEInt 1))
+
+
+--testing nested arithmetic operations
+test3 = (CEop (CEop (CEInt 10) "*" (CEInt 5)) "-" (CEop (CEInt 4) "/" (CEInt 2)))
+
+main = putStrLn (jasminWrapper (compile test3))
 
 
--- my_makefile.sh
+++ my_makefile.sh
@@ -1,4 +1,4 @@
 ghc CaseCompiler/compiler.hs
 ./CaseCompiler/compiler > program.j
 java -jar jasmin.jar program.j
-java Program/program
+java Program
