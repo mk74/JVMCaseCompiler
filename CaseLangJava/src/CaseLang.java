b1c64c1a8a2f296f54e795b9b2496b16153d1ca0
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-25 13:43:34 +0100

    Java prototype to see how JVM code looks like

---
+++ CaseLangJava/src/CaseLang.java
@@ -0,0 +1,29 @@
+//class Primitive{
+//	int type;
+//	String str;
+//	Boolean bool;
+//	int num;
+//}
+
+class ADT{
+	String tag;
+	String value;
+	ADT array[];
+}
+
+public class CaseLang {
+
+	public static void main(String[] args) {
+		int a = 2+2;
+		System.out.println(a);
+//		ADT t= new ADT();
+//		t.tag = "Min";
+//		t.value = "2";
+//		t.tag = "Time";
+//		t.array= new ADT[1];
+//		t.array[0] = new ADT();
+//		t.array[0].tag = "Min";
+//		t.array[0].value = "2";
+	}
+
+}
