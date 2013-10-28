6988230bff9e9d13630877f34c1a1c5fcffd8af3
Martin M. Kukla <mk74@st-andrews.ac.uk>
2013-10-28 21:40:01 +0000

    keeping state of stack (what type is there)

--- CaseCompiler/compiler.hs
+++ CaseCompiler/compiler.hs
@@ -37,20 +37,27 @@ get_type id1 env = fst (find id1 env)
 get_local_var :: String -> Env -> String
 get_local_var id1 env = show (snd (find id1 env))
 
+start_env :: Env
+start_env = [("", ("", -1) )]
+
+track_stack :: CType -> Env -> Env
+track_stack type1 env1 = [("", (type1, -1) )] ++ (drop 1 env1)
+
 compile_str :: Env ->CExpr -> String
 compile_str env expr = snd (compile env expr)
 
 compile :: Env -> CExpr -> (Env, String)
-compile env (CEInt i1) = (env, "sipush " ++ show i1 ++ "\n")
-compile env (CEId id1) = (env, (load_instr (get_type id1 env) ) ++ (get_local_var id1 env) ++ "\n")
+compile env (CEInt i1) = ( (track_stack "int" env), "sipush " ++ show i1 ++ "\n")
+compile env (CEId id1) = ( (track_stack type1 env), (load_instr type1 ) ++ (get_local_var id1 env) ++ "\n")
+								where type1 = get_type id1 env
 compile env (CConst id1 e1) = (env, (new_adt id1 (compile_str env e1) ) )
 --compile (CEBool b1) = show b1
 --compile (CEString str1) = str1
 
 compile env (CEOp e1 op1 e2) = (env, (compile_str env e1) ++ (compile_str env e2) ++ (op_func op1) ++ "\n")
 
-compile env (CENewVar id1 t1 e1) =  (env', (compile_str env e1) ++ (store_instr t1) ++ show ((length env) + 2) ++ "\n")
-														where env' = [(id1, (t1, (length env) + 2) )] ++ env
+compile env (CENewVar id1 t1 e1) =  (env', (compile_str env e1) ++ (store_instr t1) ++ show ((length env) + 1) ++ "\n")
+														where env' = [(id1, (t1, (length env) + 1) )] ++ env
 
 compile env (CExprs [e1]) = (env, compile_str env e1)
 compile env (CExprs (e1:es)) = ( (fst res1), (snd res1) ++ (snd (compile (fst res1) (CExprs es) ) ) )
@@ -132,7 +139,7 @@ test6 = (CExprs [(CENewVar "sth" "Age"  (CConst "Age" (CEInt 3) ) ), (CEInt 2)] 
 
 main = do
 		writeFile "adt.j" adt_class
-		putStrLn (jasminWrapper ((snd (compile [] test6) )))
+		putStrLn (jasminWrapper ((snd (compile start_env test6) )))
 
 
 
